a467a1834d3edab8333e8b741b7de5cef3760ca8
vectorzwt <zuweitao@sjtu.edu.cn>
2021-07-20 17:00:01 +0800

    update app.py html files

--- demos/template/app.py
+++ demos/template/app.py
@@ -1,34 +1,29 @@
-# -*- coding: utf-8 -*-
-"""
-    :author: Grey Li (李辉)
-    :url: http://greyli.com
-    :copyright: © 2018 Grey Li
-    :license: MIT, see LICENSE for more details.
-"""
 import os
 from flask import Flask, render_template, flash, redirect, url_for, Markup
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'secret string')
-app.jinja_env.trim_blocks = True
-app.jinja_env.lstrip_blocks = True
+app.jinja_env.trim_blocks = True  # 删除语句后第一个空行
+app.jinja_env.lstrip_blocks = True  # 删除语句行之前的空格
 
+# Create template
 user = {
-    'username': 'Grey Li',
-    'bio': 'A boy who loves movies and music.',
+    'username': 'Jacky Zu',
+    'bio': 'A boy who studys BME in SJTU and loves movies and music.',
 }
-
 movies = [
-    {'name': 'My Neighbor Totoro', 'year': '1988'},
-    {'name': 'Three Colours trilogy', 'year': '1993'},
-    {'name': 'Forrest Gump', 'year': '1994'},
-    {'name': 'Perfect Blue', 'year': '1997'},
-    {'name': 'The Matrix', 'year': '1999'},
-    {'name': 'Memento', 'year': '2000'},
-    {'name': 'The Bucket list', 'year': '2007'},
-    {'name': 'Black Swan', 'year': '2010'},
-    {'name': 'Gone Girl', 'year': '2014'},
-    {'name': 'CoCo', 'year': '2017'},
+    {
+        'name': 'The Matrix',
+        'Year': 1999
+    },
+    {
+        'name': 'Let the Bullets Fly',
+        'Year': 2010
+    },
+    {
+        'name': 'Inception',
+        'Year': 2010
+    },
 ]
 
 
@@ -42,26 +37,26 @@ def index():
     return render_template('index.html')
 
 
-# register template context handler
+# 注册模板上下文处理函数
 @app.context_processor
-def inject_info():
+def inject_foo():
     foo = 'I am foo.'
-    return dict(foo=foo)  # equal to: return {'foo': foo}
+    return dict(foo=foo)  # 在模板中直接使用foo变量
 
 
-# register template global function
+# template global function
 @app.template_global()
 def bar():
     return 'I am bar.'
 
 
-# register template filter
+# 自定义过滤器
 @app.template_filter()
 def musical(s):
-    return s + Markup(' &#9835;')
+    return s + Markup(' &#9835;')  # 音符的html符号
 
 
-# register template test
+# 自定义测试器
 @app.template_test()
 def baz(n):
     if n == 'baz':
@@ -71,23 +66,24 @@ def baz(n):
 
 @app.route('/watchlist2')
 def watchlist_with_static():
-    return render_template('watchlist_with_static.html', user=user, movies=movies)
+    return render_template('watchlist_with_static.html',
+                           user=user,
+                           movies=movies)
 
 
-# message flashing
+# 使用flash()函数实现闪现消息
 @app.route('/flash')
 def just_flash():
-    flash('I am flash, who is looking for me?')
+    flash(u'这里是祖伟韬的flask网站')  # 最好加上u
     return redirect(url_for('index'))
 
 
-# 404 error handler
+# 自定义错误界面
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('errors/404.html'), 404
 
 
-# 500 error handler
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('errors/500.html'), 500
